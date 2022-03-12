4f98b671e6c42e889b25b16ea75f6667335de58c
nolazheng <nola.zheng@zoomo.space>
2022-03-12 15:26:56 +0800

    build(npm): support clipboard copy

--- src/dashboard/components/bottom-dialog.jsx
+++ src/dashboard/components/bottom-dialog.jsx
@@ -1,31 +1,57 @@
+import { useRef } from 'react'
+import ClipboardJS from 'clipboard'
+
 import { Dialog } from 'primereact/dialog'
-import '../index.css'
 import { Button } from 'primereact/button'
+import { Toast } from 'primereact/toast'
+
+import '../index.css'
+
+const invitationCode = 'Homexin1314520'
 
 export const BottomDialog = () => {
-  const Footer = () => (
+  const toast = useRef(null)
+  const clipboard = new ClipboardJS('.clipboard-btn')
+
+  clipboard.on('success', _ => {
+    if (toast.current) {
+      toast.current.show({
+        severity: 'success',
+        summary: 'Success',
+        detail: '成功複製推薦碼',
+        life: 3000,
+      })
+    }
+  })
+
+  const Footer = (
     // TODO: custom Button UI
+
     <Button
+      className="clipboard-btn"
       label="點我複製推薦碼"
-      // onClick={() => onHide(name)}
-      className="p-button-text"
+      data-clipboard-text={invitationCode}
     />
   )
 
   return (
-    <Dialog
-      modal={false}
-      header="我的推薦碼"
-      position="bottom"
-      visible
-      footer={Footer}
-      closable={false}
-      className="dialog-bottom"
-      style={{ margin: 0 }}
-    >
-      <p>
-        我的推薦碼是Homexin1314520，快點一起來Home心接案賺錢吧！註冊成功還有100元可以領取唷！
-      </p>
-    </Dialog>
+    <>
+      <Dialog
+        modal={false}
+        header="我的推薦碼"
+        position="bottom"
+        visible
+        footer={Footer}
+        closable={false}
+        className="dialog-bottom"
+        style={{ margin: 0 }}
+      >
+        <p>
+          我的推薦碼是{invitationCode}
+          ，快點一起來Home心接案賺錢吧！註冊成功還有100元可以領取唷！
+        </p>
+      </Dialog>
+      <Toast ref={toast} style={{ width: 300 }} position="bottom-center" />
+    </>
   )
 }
