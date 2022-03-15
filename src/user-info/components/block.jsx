889a5d0cf4e47a73a8e6f290c66dc8a80fd273cb
nolazheng <nola.zheng@zoomo.space>
2022-03-15 16:53:32 +0800

    ui(form/block): add block component

---
+++ src/user-info/components/block.jsx
@@ -0,0 +1,41 @@
+export const Block = ({ label, unit, children }) => {
+  return (
+    <div style={styles.container}>
+      <div style={styles.label}>{label}</div>
+      <div style={styles.inputContainer}>
+        {children}
+        {unit && <span style={styles.unit}>{unit}</span>}
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 15,
+  },
+  label: {
+    display: 'flex',
+    alignSelf: 'flex-end',
+    borderBottom: '2px solid',
+    flex: 2,
+    justifyContent: 'center',
+    fontSize: 17,
+    marginRight: 18,
+    paddingBottom: 5,
+  },
+  inputContainer: {
+    display: 'flex',
+    flex: 3,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  unit: {
+    paddingLeft: 5,
+    whiteSpace: 'nowrap',
+  },
+}
